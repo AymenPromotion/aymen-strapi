3894fb676097a5828a0a554da0089ec445f04d7b
Belkacem <belkacemeziani007@gmail.com>
2024-05-14 16:14:18 +0100

    initial commit - strapi

---
+++ types/generated/components.d.ts
@@ -0,0 +1,21 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface SeoMetaData extends Schema.Component {
+  collectionName: 'components_seo_meta_data';
+  info: {
+    displayName: 'Meta Data';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.Text;
+    metaImage: Attribute.Media;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'seo.meta-data': SeoMetaData;
+    }
+  }
+}
