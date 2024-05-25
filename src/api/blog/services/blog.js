a448eba3f54d7dc04fa735f38870b7a740b9210c
Belkacem <belkacemeziani007@gmail.com>
2024-05-25 11:46:54 +0100

    add fetch blog by slug service

--- src/api/blog/services/blog.js
+++ src/api/blog/services/blog.js
@@ -1,9 +1,24 @@
 'use strict';
 
-/**
- * blog service
- */
-
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::blog.blog');
+module.exports = createCoreService(
+  "api::blog.blog",
+  ({ strapi }) => ({
+    async findOne(slug) {
+      const isId = Number.isInteger(parseInt(slug));
+
+      const entity = await strapi.db.query("api::blog.blog").findOne({
+        where: isId
+          ? {
+              $or: [{ slug }, { id: slug }],
+            }
+          : {
+              slug,
+            },
+      });
+
+      return entity;
+    },
+  })
+);
